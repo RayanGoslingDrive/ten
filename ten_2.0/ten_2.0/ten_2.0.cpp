22a9c459e8cddb4f9dd712e1607b763eaa2d9b89
RayanGoslingDrive <119112825+RayanGoslingDrive@users.noreply.github.com>
2023-02-28 13:56:07 +0600

    Add files via upload

---
+++ ten_2.0/ten_2.0/ten_2.0.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+	int a;
+	int count = 0;
+	srand(time(0));
+	do
+	{
+		
+		a = 1 + rand() % 1001;
+		count = count+1 ;
+		cout << count << ")" << a << endl;
+	} while (a != 10);
+	return 0;
+}
